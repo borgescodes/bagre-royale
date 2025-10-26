7d641a6f43e51d5e12a013caa367ef7bb5e01605
Pedro Borges <borgescodes@gmail.com>
2025-10-26 13:14:41 -0300

    fixes v.1.01

--- data.js
+++ data.js
@@ -5,19 +5,37 @@ window.BAGRE_DATA = {
       avatar: "assets/bagres/borges.webp",
       achievements: ["Campeão 1ª Season Bagre Royale","Campeão 1ª Edição - SÉRIE A"],
     },
+
     Lascanor: {
       name: "Lascanor",
       avatar: "assets/bagres/lascanor.webp",
       achievements: ["2º Lugar – 1ª Season Bagre Royale", "2º Lugar da 1ª Edição - SÉRIE A"],
     },
+
     Bad: {
       name: "Bad",
       avatar: "assets/bagres/bad.webp",
       achievements: ["3º Lugar – 1ª Season Bagre Royale"],
     },
-    Person: { name: "Person", avatar: "assets/bagres/person.webp", achievements: [] },
-    Gaab: { name: "Gaab", avatar: "assets/bagres/gaab.webp", achievements: [] },
-    Vegê: { name: "Vegê", avatar: "assets/bagres/vege.webp", achievements: ["3º Lugar – 1ª Edição BAGRELEIRÃO SÉRIE A"] },
+
+    Person: {
+      name: "Person",
+      avatar: "assets/bagres/person.webp",
+      achievements: [] 
+    },
+
+    Gaab: { 
+      name: "Gaab",
+      avatar: "assets/bagres/gaab.webp",
+      achievements: [] 
+    },
+
+    Vegê: {
+      name: "Vegê",
+      avatar: "assets/bagres/vege.webp",
+      achievements: ["3º Lugar – 1ª Edição BAGRELEIRÃO SÉRIE A"] 
+    },
+    
     Dan: { name: "Dan", avatar: "assets/bagres/dan.webp", achievements: [] },
     Boaz: { name: "Boaz", avatar: "assets/bagres/boaz.webp", achievements: [] },
     Felipão: { name: "Felipão", avatar: "assets/bagres/felipão.webp", achievements: [] },
